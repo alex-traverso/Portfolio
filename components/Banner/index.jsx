e25cbddce2c5faf16f302c12674f1d0c6182faea
alex traverso <alextraverso6@gmail.com>
2024-06-01 16:36:00 -0300

    fix banner animation

--- components/Banner/index.jsx
+++ components/Banner/index.jsx
@@ -5,17 +5,17 @@ import { motion } from "framer-motion";
 export default function Banner() {
 	return (
 		<>
-			<div className="relative bg-dark w-full h-[calc(100vh_-_5rem)] mt-20 flex items-center justify-start overflow-hidden">
-				<motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={{ duration: 1.5 }}
-					className="absolute z-10 flex flex-col justify-center items-start bg-black lg:px-sectionSides mm:px-sectionSidesMobile w-full h-full bg-opacity-[0.3]"
-				>
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 1.5 }}
+				className="relative bg-dark w-full h-[calc(100vh_-_5rem)] mt-20 flex items-center justify-start overflow-hidden"
+			>
+				<div className="absolute z-10 flex flex-col justify-center items-start bg-black lg:px-sectionSides mm:px-sectionSidesMobile w-full h-full bg-opacity-[0.3]">
 					<div>
 						<h1
 							className="text-white font-madeOuterBold font-bold md:text-7xl
-						mm:text-4xl m:text-5xl text-left mb-2"
+						mm:text-4xl m:text-[2.5rem] text-left mb-2"
 						>
 							ALEX TRAVERSO
 						</h1>
@@ -43,14 +43,14 @@ export default function Banner() {
 					>
 						CONTACTO
 					</Link>
-				</motion.div>
+				</div>
 
 				<div className="absolute top-0 left-0 h-screen w-screen">
 					<video autoPlay loop muted className="h-full w-full object-cover">
 						<source src="/video-banner.mp4" />
 					</video>
 				</div>
-			</div>
+			</motion.div>
 		</>
 	);
 }
